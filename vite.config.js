8da3300dc7d053a92ffd3963146e94b31130c0b4
Khushi Agnihotri <khushiagnihotri01@gmail.com>
2026-01-31 00:01:41 +0530

    Deploy Portfolio to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/Portfolio/" : "/",
+export default defineConfig({
+  base: "/Portfolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
